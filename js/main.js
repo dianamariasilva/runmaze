253764b393e827eecc917898596110d63af1a450
solics <s.huayhua.ma@gmail.com>
2017-08-07 16:55:35 -0500

    subiendo auto

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ var mapaDiv = document.getElementById("mapa");
 var izq = document.getElementById("left");
 var der = document.getElementById("right");
 var go = document.getElementById("forward");
+var buscar = document.getElementById("buscar");
 var actual;
+var muro;
 var mapa=[
 	"******************",
 	"*_________*______*",
 	"*_*****_____******",
-	"*______***__*__*_*",
-	"***_*____*____**_*",
-	"*___*____**__*___*",
-	"*_********__**_*_*",
+	"*_*___*_***_*__*_*",
+	"*_*_*_*_*_____**_*",
+	"***_*___*_*__*___*",
+	"*___******__**_*_*",
 	"*____*______*__*_*",
-	"*_**_*__*****_**_*",
-	"*o*__*________**W*",
+	"*_**_*_******_**_*",
+	"*o*___________**W*",
 	"******************"];
 var celdas = new Array(mapa.length);
 for (var i=0;i<mapa.length;i++)
@@ -42,7 +44,11 @@ function dibujarMapa() {
 		tabla.appendChild(tr);
 	}
 	mapaDiv.appendChild(tabla);
+	var casa = document.createElement("img");
+	casa.src= "img/home.png";
+	celdas[10][1].appendChild(casa);
 }
+
 
 function jugar(){
 	var img = document.createElement("img");
@@ -106,6 +112,8 @@ go.onclick = function(){
 			actual.direccion = dir_actual;
 			actual.td.appendChild(img);
 		}
+		else if(mapa[actual.x-1][actual.y]== "*" )
+			muro = {flag:true,direccion:0}
 	}
 	else if(dir_actual== 1){
 		if(mapa[actual.x][actual.y+1] == "_" || mapa[actual.x][actual.y+1] == "W"){
@@ -115,6 +123,8 @@ go.onclick = function(){
 			actual.direccion= dir_actual;
 			actual.td.appendChild(img);
 		}
+		else if(mapa[actual.x][actual.y+1]== "*" )
+			muro = {flag:true,direccion:1}
 	}
 	else if(dir_actual== 2){
 		if(mapa[actual.x+1][actual.y] == "_" || mapa[actual.x+1][actual.y] == "W"){
@@ -124,6 +134,8 @@ go.onclick = function(){
 			actual.direccion= dir_actual;
 			actual.td.appendChild(img);
 		}
+		else if(mapa[actual.x+1][actual.y]== "*" )
+			muro = {flag:true,direccion:2}
 	}
 	else if(dir_actual== 3){
 		if(mapa[actual.x][actual.y-1] == "_" || mapa[actual.x][actual.y-1] == "W"){
@@ -133,17 +145,50 @@ go.onclick = function(){
 			actual.direccion= dir_actual;
 			actual.td.appendChild(img);
 		}
+		else if(mapa[actual.x][actual.y-1]== "*" )
+			muro = {flag:true,direccion:3};
 	}
 
 	if(mapa[actual.x][actual.y] == "W" ){
 		actual.td.removeChild(actual.td.firstChild);
-		img.src =  "img/win.png";
-		actual.td.appendChild(img);
-
+		//img.src =  "img/"+d+".png";
+		actual.appendChild(img)
 		celdas=[];
 		actual=undefined;
 	}
 };
+
+
+var timerr;
+buscar.onclick = function(){
+	timerr = setInterval(buscarSalida,200);
+};
+var direccion=actual.direccion-1;
+function buscarSalida(){
+	if(actual==undefined)
+		clearInterval(timerr);
+	if(muro !=undefined){
+		actual.td.removeChild(actual.td.firstChild);
+		var img=document.createElement("img");
+		img.src="img/"+ direccion +".png";
+		actual.td.appendChild(img);
+		actual.direccion=direccion;
+		muro=undefined;
+		direccion++;
+		direccion = direccion % 4;
+	}
+	else{
+		go.click();
+		if(muro ==undefined && actual !=undefined){
+			direccion = actual.direccion-1;
+			if(direccion ==-1)
+				direccion = 3;
+		}
+	}
+}
+
+
+
 
 window.onload = function(){
 	document.onkeyup = function(event){
@@ -154,4 +199,4 @@ window.onload = function(){
 		if(event.keyCode == 39)
 			der.click();
 	}
-}
+};
